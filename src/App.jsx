d24365ec49690e78b5ef7a497859a6d038d28b0f
Bruno B. Silva <contato.brunobsilva@outlook.com>
2026-06-04 20:26:13 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -35,36 +35,6 @@ function App() {
   const hideDefaultLayoutOn = ['/login', '/register', '/dashboard']
   const hideDefaultLayout = hideDefaultLayoutOn.some(path => location.pathname.startsWith(path))
 
-<<<<<<< HEAD
-  return (
-    <ThemeProvider>
-      <>
-        <SmoothScroll />
-        {!hideDefaultLayout && <Navbar />}
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/pages/sobre" element={<Sobre />} />
-          
-          {/* Dashboard routes */}
-          <Route path="/dashboard" element={<Dashboard />}>
-            <Route index element={<DashboardHome />} />
-            <Route path="receitas" element={<DashboardReceitas />} />
-            <Route path="pedidos" element={<DashboardPedidos />} />
-            <Route path="tratamentos" element={<DashboardTratamentos />} />
-            <Route path="lembretes" element={<DashboardLembretes />} />
-            <Route path="notificacoes" element={<DashboardNotificacoes />} />
-            <Route path="historico" element={<DashboardHistorico />} />
-            <Route path="configuracoes" element={<DashboardConfiguracoes />} />
-            <Route path="ajuda" element={<DashboardAjuda />} />
-          </Route>
-        </Routes>
-        {!hideDefaultLayout && <Footer />}
-      </>
-    </ThemeProvider>
-  )
-=======
     return (
   <ThemeProvider>
     <>
@@ -95,7 +65,6 @@ function App() {
     </>
   </ThemeProvider>
 )
->>>>>>> 274a2b83356d1baeefa5d5c3d847c87ab2a1c817
 }
 
 export default App
